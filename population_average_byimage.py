51d620c992c5cd86f1ab7a901c93c369cbe4e8cd
Torrey <salmont3@gmail.com>
2016-09-01 21:53:45 -0700

    updated minimally

--- population_average_byimage.py
+++ population_average_byimage.py
@@ -66,10 +66,7 @@ for struct in targeted_structures:
 list_of_dicts = [{'area':area, 'list_of_arrays':np.asarray(tuple(A))} for area, A in zip(targeted_structures, mean_bystructure)]
 np.save('mean_of_mean_population', list_of_dicts)
 # In[]
-    #testing area
-#boc.get_experiment_containers?
-#for A in list_of_arrays:
-#    print(A.shape)  
+#testing area 
 
 # In[]
 #make a plot for each structure
@@ -77,13 +74,89 @@ np.save('mean_of_mean_population', list_of_dicts)
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 30})  
 tuple_list = [(i, j) for i in range(2) for j in range(2)]
+idx=[0, 1, 2, 3]
 fig, axes = plt.subplots(2,2)
-for t, d, in zip(tuple_list, list_of_dicts):
+#axes.set_yticks([])
+#axes.yaxis.set_ticklabels([])
+for t, d, i in zip(tuple_list, list_of_dicts, idx):
     traces=d['list_of_arrays'].T
-    axes[t[0], t[1]].plot(traces + 2 * np.arange(traces.shape[1]).reshape(1,traces.shape[1]))
-    axes[t[0], t[1]].set(ylabel = 'Mean Sweep Response', xlabel='Image Number', title=d['area'])
-#fig.set(title = 'Mean of Mean Population Response')
+    axes[t[0], t[1]].plot(traces + 2 * np.arange(traces.shape[1]).reshape(1,traces.shape[1]), color='g', linewidth=2)
+    axes[t[0], t[1]].set(ylabel = 'Pop Mean Sweep Response', xlabel='Image Number', title=d['area'])
+    axes[t[0], t[1]].plot(grand_mean[i] + -2, color='k', linewidth=5)
+    axes[t[0], t[1]].axvline(x=31)
+    axes[t[0], t[1]].set_yticks([])
+
 plt.suptitle('Mean of Mean Population Response')
   
 # In[]
+# Remove one experiment that is really variable from VISal
+
+#popmean_corrected_VISal = list_of_dicts{'area'='VISal', 'list_of_arrays'=[0,1,2,3,4,5,6,7,9,10,11,12,13,14,15,16]}  
+test=list_of_dicts[area == 'VISal']
+VISal_array=test['list_of_arrays']
+VISal_list_corrected= np.delete(VISal_array, 8, 0)
+
+test['list_of_arrays']=VISal_list_corrected
+list_of_dicts[0]['list_of_arrays']=VISal_list_corrected
+
+# In[]
+# Generate mean of each structure to get a sense of images liked/disliked
+grand_mean=[]
+for i in range(len(list_of_dicts)):
+    grand_mean.append(np.mean(list_of_dicts[i]['list_of_arrays'], axis=0))
+
+# In[]
+# locate images that a structure really cares about
+image_w_popresponse=[]
+for i in range(len(grand_mean)):
+    image_locs= grand_mean[i] # > np.mean(grand_mean[i], axis=1)
+    #image_w_popresponse.append()
+# In[]
+# Gather info about variability. 
+
+# replicate for loop above:
+targeted_structures = boc.get_all_targeted_structures()
+#targeted_structure=targeted_structures[3]     
+expt_cont_list = pd.DataFrame(boc.get_experiment_containers())
+#expt_cont_ids=[511511015, 511510733]
+
+mean_bystructure=[]
+all_sorted_img_responses=[]
+all_bycell_mean=[]
+for struct in targeted_structures:
+    expt_cont_VISlist=expt_cont_list[expt_cont_list['targeted_structure']==struct]
+    expt_cont_ids=expt_cont_VISlist['id'].unique()
+    
+    mean_by_container=[] 
+    all_by_cell_mean=[]
+    sorted_img_responses=[]
+    cell_means_byimage=[]
+    for cont_id in expt_cont_ids:
+        session_id, session_data=get_session_id(cont_id, 'B', boc=boc)
+        response, mean_sweep_response, sweep_response, stim_table_ns = open_h5_file_sessionID(session_id, 'd:', 'B') 
+        
+        mean_sweep_response_nd = mean_sweep_response.values 
+        mean_perstim=[] 
+        img_responses=[]
+        img_ids = sorted(stim_table_ns['frame'].unique())
+        
+        for stim_num in img_ids:
+            img_idx=stim_table_ns['frame']==stim_num
+            all_img_data=mean_sweep_response[img_idx]
+            bycell_mean=all_img_data.mean()
+            img_responses.append(all_img_data)
+            
+            #by_mean=small_mean.mean()
+        print('container id: ', cont_id)
+        sorted_img_responses.append(img_responses)
+        cell_means_byimage.append(bycell_mean)
+        mean_by_container.append(mean_perstim)    
+    mean_bystructure.append(mean_by_container)
+    all_sorted_img_responses.append(sorted_img_responses)
+    all_bycell_mean.append(cell_means_byimage)
+
+
+
+
+    
 #do some statistics
