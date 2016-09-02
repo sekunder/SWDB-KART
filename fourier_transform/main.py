13c5bd9fea992ff60ab8a784976a139407a93300
Alex Kunin <abk170@psu.edu>
2016-09-01 23:16:01 -0700

    cleaned up FFT function, added new placeholder
    I need to work out the details of how to get the "orientation" from the
    FFT'd image

--- fourier_transform/main.py
+++ fourier_transform/main.py
@@ -10,39 +10,23 @@ def fourier_transform(image):
 	F1 = fftpack.fft2(image - np.mean(image[:]))  # shift them quadrants around so dat low spatial frequencies are da center of da 2D fourier transformed image.
 	F2 = fftpack.fftshift(F1)  # Calculatin' a 2D power spectrum
 	psd2D = np.abs(F2) ** 2  # Calculate the azimuthally averaged 1D power spectrum
-	psd1D = radialProfile.azimuthalAverage(psd2D, center=[459, 587])
-	# plottin dem results ya'll!
-	img1 = np.log10(image + 1)
-	img2 = np.log10(psd2D)
-	img3 = psd1D
-	if plot:
-		plt.figure(1)
-		plt.imshow(img1, cmap="gray")  # The +1 shifts it to non-zero values
-		plt.figure(2)
-		plt.imshow(img2, cmap="jet")
-		plt.figure(3)
-		plt.semilogy(img3)
-		plt.title('1D Power Spectrum')
-		plt.xlabel('Spatial Frequency')
-		plt.ylabel('Power Spectrum')
-	return img1, img2, img3
+	# psd1D = radialProfile.azimuthalAverage(psd2D, center=[459, 587])
+	log_image = np.log10(image + 1)
+	fft_image = np.log10(psd2D)
+	power_spectrum = radialProfile.azimuthalAverage(psd2D, center=[459, 587])
+	# if plot:
+	# 	plt.figure(1)
+	# 	plt.imshow(log_image, cmap="gray")  # The +1 shifts it to non-zero values
+	# 	plt.figure(2)
+	# 	plt.imshow(fft_image, cmap="jet")
+	# 	plt.figure(3)
+	# 	plt.semilogy(power_spectrum)
+	# 	plt.title('1D Power Spectrum')
+	# 	plt.xlabel('Spatial Frequency')
+	# 	plt.ylabel('Power Spectrum')
+	return log_image, fft_image, power_spectrum
 
-#
-# image_transforms = {}  # this is a dictionary
-# for i in range(natural_scenes.shape[0]):
-# 	tmp = {}
-# 	image = natural_scenes[i, :, :]
-# 	img1, img2, img3 = fourier_transform(image)
-# 	image_transforms[i] = {
-# 		'fourier_trans_img': img1,
-# 		'processed_image': img2,
-# 		'power_spec': img3,  # when retrieving this use plt.semilogy as seen in the fouri_trans function
-# 	}
-# # the units are cycles per image
-
-
-#show me dat natural scene!
-#use this if you just want to look at a single image
-# img = natural_scenes[113,:,:]
-# plt.imshow(img,cmap= "gray")
-# fouri_trans(img,plot=True)
+def image_orientation(image):
+	"""computes an image's 'orientation' according to [TODO: describe metric]"""
+	# TODO what goes here?
+	return 1.0
