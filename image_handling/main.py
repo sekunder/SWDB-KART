7843d8b76a883fbd1039f7d11a73385046717a0e
Alex Kunin <abk170@psu.edu>
2016-09-05 14:26:47 -0500

    merged fourier_transform package into image_handling
    In a future update I will remove the fourier_transform directory, since
    that package ended up doing more image-related things than fourier
    transform-related things

--- image_handling/main.py
+++ image_handling/main.py
@@ -1,153 +1,86 @@
 import os
-import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter
-from scipy import misc
+import numpy as np
+from scipy import fftpack
+from scipy.ndimage import map_coordinates
 
-def get_responsive_cells(ns,thresh=0.5):
-	"""Return cells from the given NS object that have mean response to preferred condition greater than some threshold"""
-	responsive_cells = ns.peak[ns.peak.peak_dff_ns >= thresh].cell_specimen_id.values
-	return responsive_cells
+def log_fourier_transform(image):
+	"""Shifts the supplied image so its mean intensity is 0, then computes the log of the absolute value of the fourier transform.
+	Input:
+		:numpy.ndarray image : the image to transform
+	Output:
+		:numpy.ndarray mean_shifted_image : the image with mean shifted to 0
+		:numpy.ndarray fft_image : The image, passed through the following transformations (in order): shift mean to 0, FFT, FFTshift, norm squared, log10
+			See scipy.fftpack.fft2 and scipy.fftpack.fftshift"""
+	mean_shifted_image = image - np.mean(image[:])
+	shifted_fft_raw_image = fftpack.fftshift(fftpack.fft2(mean_shifted_image))
+	log_fft_image =  np.log10(np.abs(shifted_fft_raw_image) ** 2 + 1) # add one to take care of values too close to 0
+	return mean_shifted_image, log_fft_image
+
+def radial_image_intensity(image, x_0, y_0, r, theta=(0.0, np.pi / 4, np.pi / 2, 3 * np.pi / 4), num_interp_points=None):
+	"""returns the image's 'radial intensity' around the given point, to a distance of r, for each angle in the iterable theta.
+	Input:
+		:numpy.ndarray image : The image to transform
+		:float x_0, y_0 : The coordinates from which the radial intensity will be computed
+		:float r : The distance to integrate along
+		:iterable theta : A list, array, or other iterable collection of floats, which are the angles in which to integrate.
+	Output:
+		theta : the input theta, or the default of 45 degree increments from 0 (inclusive) to 180 (exclusive)
+		intensity : the intensity in the corresponding direction. The idea is you can just say plot(theta, intensity) and get something useful."""
+	intensity = [image_line_segment_intensity(image, x_0, y_0, x_0 + r * np.cos(t), y_0 + r * np.sin(t), num_steps=num_interp_points) for t in theta]
+	return theta, intensity
+
+def image_line_segment_intensity(image, x_0, y_0, x_1, y_1, num_steps=None):
+	"""computes the 'mean intensity' of the image along the line segment from (x0,y0) to (x1, y1).
+	This is defined as 1/r times the sum of the pixel intensities on the interpolated line between the two endpoints (inclusive)
+
+	Input:
+		image : ndarray image
+		x0, y0 : the (pixel) coordinates of the start point
+		x1, y1 : the (pixel) coordinates of the end point
+
+	Output:
+		mean intensity
+
+	Note:
+		This is undoubtedly going to be rather crude, because we are interpolating from pixel to pixel. To improve accuracy, use a large number (around 500) num_steps. To improve accuracy further, improve the interpolated_pixel_values function to do an honest-to-goodness integral along a piecewise constant function."""
+	r = np.hypot(np.abs(x_0 - x_1), np.abs(y_0 - y_1))
+	int_pixels,ds = interpolated_pixel_values(image, x_0, y_0, x_1, y_1, num_steps=num_steps)
+	total_intensity =  np.sum(ds * int_pixels)
+	return float(total_intensity) / r
 
 
-def get_ns_response_arrays(data_set,responsive_cells,stim_table):
-	"""Returns arrays of imaging frames, stimulus frames, and dF/F trace values for 
-         the frames during the experiment_session where natural scenes were shown. Specifically: 
-         frames_arr: indices of frames during the experiment when natural scenes were shown, 
-                     created using stim_table.start and stim_table.end times
-         images_arr: natural_scenes image id for frames when natural scenes were shown, 
-                     created using stim_table.frame values 
-         traces_arr: dF/F value for frames during the experiment when natural scenes were shown,
-                     for cells in responsive_cells. 
-         
-         all arrays should have the same length """
-	timestamps,traces = data_set.get_dff_traces(cell_specimen_ids=responsive_cells)
+def interpolated_pixel_values(image, x_0, y_0, x_1, y_1, num_steps=None, ord=1):
+	"""returns the pixel values along the interpolated line between the (pixel) coordinates in the given image, using interpolation of order ord. Uses scipy.ndimage.map_map_coordinates to do the interpolation.
+	Input:
+		image : image as ndarray
+		x_0, y_0 : start coordinates
+		x_1, y_1 : end coordinates
+		num_steps : number of sample points to take in the middle. By default, this is the ceiling of the distance between the start and end point
+		ord : order of interpolation
+	Output:
+		pixel_values : the image intensity at each sample point
+		ds : the step size, for taking Riemann sums
 
-	frames_arr = np.empty(0)
-	images_arr = np.empty(0)
-	for sweep in range(len(stim_table)):
-		start = stim_table.iloc[sweep].start
-		end = stim_table.iloc[sweep].end
-		frames = np.arange(start,end)
-		frames_arr = np.hstack((frames_arr,frames))
-		image = stim_table.iloc[sweep].frame
-		for i in range(len(frames)):
-			images_arr = np.hstack((images_arr,image))
+	Note:
+		To improve numerical performance, this should be rewritten using the following algorithm:
+			1. Compute the points of intersection of the line segment with the horizontal and vertical lines where at least one coordinate is an integer.
+			2. Sort those points by the order in which they are hit while traveling from the initial point to the final point
+			3. for each sub-interval, return its pixel value and length in the arrays pixel_values, ds
+		"""
+	if not num_steps:
+		num_steps = 200.0
+	dx = (x_1 - x_0)/num_steps
+	dy = (y_1 - y_0)/num_steps
+	ds = np.hypot(dx,dy)
+	step_range = np.arange(0,num_steps)
+	x_range = x_0 + step_range * dx
+	y_range = y_0 + step_range * dy
+	pixel_values = map_coordinates(image, np.vstack((x_range, y_range)),order=ord)
+	return pixel_values, ds
 
-	traces_arr = np.empty((traces.shape[0],frames_arr.shape[0]))
-	for t in range(traces.shape[0]):
-		trace = np.empty(0)
-		for sweep in range(len(stim_table)):
-			start = stim_table.iloc[sweep].start
-			end = stim_table.iloc[sweep].end
-			tmp = traces[t,start:end]
-			trace = np.hstack((trace,tmp))
-		traces_arr[t,:] = trace
-	return frames_arr, images_arr, traces_arr
-
-
-def get_calcium_triggered_image_stack(responsive_cell_idx, cell_specimen_id, ns, images, images_arr, traces_arr, thresh=0.5):
-	""" Inputs:
-             responsive_cell_idx = index of cell in list reponsive_cells (ex: 10)
-             cell_specimen_id = id of cell corresponding to responsive_cell_idx (ex: 517488841)
-             ns = NaturalScenes data_set object
-             images = array of natural_scenes from data_set.get_stimulus_template('natural_scenes')
-             images_arr = array of image ids for every frame where natural_scenes were shown
-             traces_arr = array of dF/F values for every frame where natural_scenes were shown, for every cell in responsive_cells
-             thresh = threshold dF/F value used to identify cell responses
-        Returns: 
-            cell_idx = index of cell_specimen_id 
-            pref_scene = preferred image for this cell, from ns.response array
-            img_stack = stack of images that were present 6 frames prior to a repsonse
-            thresh_vals = values of dF/F trace where response was > threshold
-            n_image = number of images that were present when the cell responded > threshold 
-            condition_mean = average trace across all sweeps of the preferred condition for this cell
-            t = array of frames corresponding to condition_mean trace
-            t_int = array of frames for condition_mean trace in increments of 6 frames, used for plotting
-            t_int_ref = array of frames for condition_mean trace in increments of 6 frames, referenced to stimulus onset time, used for plotting
-    """
-	cell_idx = np.where(ns.cell_id == cell_specimen_id)[0][0]
-
-	pref_scene = ns.peak[ns.peak.cell_specimen_id == cell_specimen_id].scene_ns.values[0]
-	pref_scene_sweeps = ns.stim_table[ns.stim_table.frame == pref_scene].index.values
-
-	condition_mean = ns.sweep_response[str(cell_idx)].iloc[pref_scene_sweeps].mean()
-	frames = ns.sweeplength + ns.interlength * 2
-	t = np.arange(0, frames)
-	t_int = np.arange(0, frames, 6)
-	t_int_ref = t_int - ns.interlength
-
-	thresh_inds = np.where(traces_arr[responsive_cell_idx, :] >= thresh)[0]
-	thresh_inds -= 6
-	thresh_vals = traces_arr[responsive_cell_idx][thresh_inds]
-	thresh_images = images_arr[thresh_inds]
-	n_images = len(np.unique(thresh_images))
-	img_stack = np.empty((thresh_images.shape[0], images[0, :, :].shape[0], images[0, :, :].shape[1]))
-	for i, img in enumerate(thresh_images):
-		img_stack[i, :, :] = images[img, :, :]
-	return cell_idx, pref_scene, img_stack, thresh_vals, n_images, condition_mean, t, t_int, t_int_ref
-
-def mean_image(img_stack,weights=None):
-	"""Compute the mean image of an image stack, which is a 3 dimensional ndarray; 
-         the first index of img_stack is the image index and the other two are width and height of the image in pixels.
-         If weights are supplied, takes the weighted average. Weights are the values of dF/F when the cell responded to the corresopnding image
-         Returns a single image which is the (weighted) mean."""
-	if weights is None:
-		return np.mean(img_stack,axis=0)
-	else:
-		return np.average(img_stack,axis=0,weights=weights)
-
-def plot_ns_summary(cell_specimen_id, responsive_cell_id, ns, images, frames_arr, images_arr, traces_arr, thresh=0.5,
-					weighted=False, save_dir=None):
-	""" For a given cell, plot a summary figure containing the mean response to the preferred condition, the preferred image,
-         the mean response to all conditions, and the (weighted) mean of all images that were present prior to a response. 
-         """
-	cell_idx, pref_scene, img_stack, thresh_vals, n_images, condition_mean, t, t_int, t_int_ref\
-		= get_calcium_triggered_image_stack(cell_specimen_id, responsive_cell_id, ns, images, images_arr,traces_arr,thresh)
-	if weighted:
-		mean_img = mean_image(img_stack, weights=thresh_vals)
-		# mean_img = np.average(img_stack, axis=0, weights=thresh_vals) / np.mean(images, axis=0)
-	else:
-		mean_img = mean_image(img_stack)
-		# mean_img = np.mean(img_stack, axis=0) / np.mean(images, axis=0)
-
-	condition_response = ns.response
-	mean_image_responses = condition_response[:, cell_idx, 0]  # [image,cell,mean]
-
-	fig, ax = plt.subplots(2, 2, figsize=(15, 10))
-	ax = ax.ravel()
-	ax[0].plot(t, condition_mean)
-	ax[0].set_xticks(t_int)
-	ax[0].set_xticklabels(t_int_ref / 30.)
-	ax[0].set_xlabel('time after stimulus onset')
-	ax[0].set_ylabel('dF/F')
-	ax[0].set_title('cell ' + str(cell_idx) + ' mean response to pref condition')
-
-	ax[1].imshow(images[pref_scene, :, :], cmap='gray')
-	ax[1].set_title('pref_im: ' + str(pref_scene))
-	ax[1].axis('off')
-
-	ax[2].plot(mean_image_responses)
-	ax[2].set_xlabel('image #')
-	ax[2].set_ylabel('mean dF/F')
-	ax[2].set_title('cell ' + str(cell_specimen_id) + ' normalized mean response to all conditions')
-
-	ax[3].imshow(mean_img, cmap='gray')
-	ax[3].axis('off')
-	if weighted:
-		ax[3].set_title('weighted mean of ' + str(n_images) + ' image conditions')
-	else:
-		ax[3].set_title('mean of ' + str(n_images) + ' image conditions')
-	plt.tight_layout()
-	if save_dir is not None:
-		if not os.path.exists(save_dir):
-			os.mkdir(save_dir)
-		save_path = os.path.join(save_dir, str(cell_specimen_id) + '.png')
-		fig.savefig(save_path)
-		save_matrix_as_image(mean_img, str(cell_specimen_id) + '_avgstim.png', save_path=save_dir)
-	# ax[4:5].plot(traces[cell_idx,:])
-
-def save_matrix_as_image(m, filename, save_path='', file_extension='png', color_map='gray'):
+def save_matrix_as_image(matrix, filename, save_path='', file_extension='png', color_map='gray'):
 	"""Saves the given matrix as an image file without axes or excess space around it.
         Input:
         	m : a 2-dimensional ndarray
@@ -159,60 +92,11 @@ def save_matrix_as_image(m, filename, save_path='', file_extension='png', color_
 	ax = plt.Axes(fig, [0., 0., 1., 1.])
 	ax.set_axis_off()
 	fig.add_axes(ax)
-	ax.imshow(m, aspect='auto', cmap=color_map)
+	ax.imshow(matrix, aspect='auto', cmap=color_map)
 	fig.savefig(os.path.join(save_path,filename + '.' + file_extension))
-	fig.clear()
+	plt.close(fig)
 
 def gaussian_blur(img,sigma=19.6):
 	"""Applies a gaussian blur to given image and returns the result. 
          By default, sigma is 19.6, which is roughly equal, in pixels, to 2 degrees of the mouse's visual field (ignoring the warping from spherical correction)"""
 	return gaussian_filter(img,sigma)
-
-def load_image_from_file(filename):
-	image = misc.imread(filename)
-	return image
-
-#
-# def plot_mean_image(cell_specimen_id,ns,images,images_arr,traces_arr,thresh=0.3,weighted=False):
-# 	# cell_idx = np.where(ns.cell_id==cell_specimen_id)[0][0]
-# 	# thresh_inds = np.where(traces_arr[cell_idx,:]>=thresh)[0]
-# 	# thresh_inds -= 6
-# 	# thresh_images = images_arr[thresh_inds]
-# 	# n_images = len(np.unique(thresh_images))
-# 	# img_stack = np.empty((thresh_images.shape[0],images[0,:,:].shape[0],images[0,:,:].shape[1]))
-# 	#
-# 	# thresh_vals = traces_arr[cell_idx][thresh_inds]
-# 	#
-# 	# for i,img in enumerate(thresh_images):
-# 	# 	img_stack[i,:,:] = images[img,:,:]
-# 	img_stack,thresh_vals,n_images = get_calcium_triggered_image_stack(cell_specimen_id, ns, images, images_arr, traces_arr, thresh)
-#
-# 	if weighted:
-# 		mean_img = mean_image(img_stack,thresh_vals)
-# 		# mean_img = np.average(img_stack,axis=0,weights=thresh_vals)
-# 	else:
-# 		mean_img = mean_image(img_stack)
-# 		# mean_img = np.mean(img_stack,axis=0)
-#
-# 	fig,ax=plt.subplots()
-# 	ax.imshow(mean_img,cmap='gray')
-# 	if weighted:
-# 		ax.set_title('cell '+str(cell_specimen_id)+', weighted mean of '+str(n_images)+' images')
-# 	else:
-# 		ax.set_title('cell '+str(cell_specimen_id)+', mean of '+str(n_images)+' images')
-#
-#
-#
-# def get_calcium_triggered_image_stack(cell_specimen_id, ns, images, images_arr,traces_arr,thresh=0.3):
-# 	"""Returns a stack of images that seem to trigger responses"""
-# 	cell_idx = np.where(ns.cell_id == cell_specimen_id)[0][0]
-# 	thresh_inds = np.where(traces_arr[cell_idx,:] >= thresh)[0]
-# 	thresh_inds -= 6
-# 	thresh_vals = traces_arr[cell_idx][thresh_inds]
-# 	thresh_images = images_arr[thresh_inds]
-# 	n_images = len(np.unique(thresh_images))
-# 	img_stack = np.empty((thresh_images.shape[0], images[0, :, :].shape[0], images[0, :, :].shape[1]))
-# 	for i, img in enumerate(thresh_images):
-# 		img_stack[i, :, :] = images[img, :, :]
-# 	return img_stack,thresh_vals,n_images
-#
